4c037eb30997a9be354f1f90f945bb3e8a96d8dd
BuildTools <unconfigured@null.spigotmc.org>
2015-05-17 14:12:49 +0100

    Added disable message.

--- src/me/harrysmc/craftingplus/CraftingPlus.java
+++ src/me/harrysmc/craftingplus/CraftingPlus.java
@@ -8,18 +8,18 @@ import java.util.logging.Logger;
 /**
  * Created by HarrysMc on 17/05/2015.
  */
+
 public class CraftingPlus extends JavaPlugin {
 
     Logger log = Bukkit.getLogger();
     @Override
     public void onEnable(){
-
         log.info("Crafting recipes set up.");
     }
 
     @Override
     public void onDisable(){
-
+        log.info("Crafting recipes disabled.");
     }
 
 }
